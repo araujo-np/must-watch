dfb375c50bcda22c6e7588e178f7c501f286d6d5
araujo-np <nathaly.paula@aluno.senai.br>
2026-03-09 16:42:55 -0300

    commit inicial

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from api-series!")
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"mensagem": "Olá, mundo!"}
+
+@app.get("/itens/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/soma/{num1}/{num2}")
+async def somar(num1: int, num2: int):
+    return {"Resultado": num1 + num2}
 
 
-if __name__ == "__main__":
-    main()
+# Crie uma rota que retorne a soma de dois números passados por caminho
+# (path de url)
+
+# Extra: melhore a tipagem do código usando tipos de mófulo typing onde for 
+# necessário
