acddd8ae6436d3ce8bee92662957dec812f64404
araujo-np <nathaly.paula@aluno.senai.br>
2026-03-16 16:42:56 -0300

    commit inicial

---
+++ app/route/serie.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.model.serie import SerieModel
+from app.schema.serie import SerieSchema
+
+serie = APIRouter()
+
+@serie.post("/")
+async def criar_serie(dados: SerieSchema, db: Session = Depends(get_db)):
+    nova_serie = SerieModel(**dados.model_dump())
+    db.add(nova_serie)
+    db.commit()
+    db.refresh(nova_serie)
+    return nova_serie
+
+@serie.get("/")
+async def listar_series(db: Session = Depends(get_db)):
+    return db.query(SerieModel).all()
